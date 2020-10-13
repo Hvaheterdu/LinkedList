2192bec0b5d1b21291257a733c14efa8c5a91708
Burhan Sarfraz <36539269+Hvaheterdu@users.noreply.github.com>
2020-10-13 23:29:31 +0200

    Add files via upload

---
+++ src/Methods.java
@@ -0,0 +1,20 @@
+interface Methods<T> extends Iterable<T> {
+
+    int size();
+
+    boolean contains(T x);
+
+    void insertPos(int pos, T x);
+
+    void insert(T x);
+
+    void set(int pos, T x);
+
+    T get(int pos);
+
+    T removePos(int pos);
+
+    T remove();
+
+    T removeLast();
+}
